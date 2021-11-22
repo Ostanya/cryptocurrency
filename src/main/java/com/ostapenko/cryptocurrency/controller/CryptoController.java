ff36cc7f8f8d00a50d53e933826b29e4133da31d
Osttanya <ostania.tania@gmail.com>
2021-11-22 19:58:57 +0200

    added loggers

--- src/main/java/com/ostapenko/cryptocurrency/controller/CryptoController.java
+++ src/main/java/com/ostapenko/cryptocurrency/controller/CryptoController.java
@@ -2,38 +2,66 @@ package com.ostapenko.cryptocurrency.controller;
 
 import com.ostapenko.cryptocurrency.entity.Crypto;
 import com.ostapenko.cryptocurrency.service.CryptoCurrService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static com.ostapenko.cryptocurrency.service.TimerService.*;
 
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/cryptocurrencies")
 public class CryptoController {
     private static List<String> list = Arrays.asList(BTC, XPR, ETH);
 
     private CryptoCurrService cryptoCurrService;
 
+    @Autowired
+    public CryptoController(CryptoCurrService cryptoCurrService) {
+        this.cryptoCurrService = cryptoCurrService;
+    }
+
     @GetMapping(value = "/minprice")
-    public double getMinPrice(String name) {
-        double minPrice = cryptoCurrService.g
+    public double getMinPrice(@RequestParam String name) {
+        if(list.contains(name)) {
+            throw new RuntimeException("Wrong request parameters");
+        }
+        double minPrice = cryptoCurrService.getMinPrice(name);
+       // log.info("Find min price");
         return minPrice;
     }
 
-    public double getMaxPrice() {}
+    @GetMapping(value = "/maxprice")
+    public double getMaxPrice(@RequestParam String name) {
+        if(list.contains(name)) {
+            throw new RuntimeException("Wrong request param");
+        }
+        double maxPrice = cryptoCurrService.getMaxPrice(name);
+        //log.info("Find max price");
+        return maxPrice;
+    }
 
     @GetMapping
-    public Page<Crypto> getNameOfCryptocurrencies(String name, int page, int size) {
+    public Page<Crypto> getNameOfCryptocurrencies(@RequestParam String name,
+                                                  @RequestParam int page,
+                                                  @RequestParam int size) {
         if(page < 0 || size <= 0 || list.contains(name)) {
             throw new RuntimeException("Wrong request parameters");
         }
 
         Pageable pageable = PageRequest.of(page, size, Sort.by("lprice"));
-        log.info("Find list of {} cryptocurrencies records", name);
-        return ;
+       // log.info("Find list of {} cryptocurrencies records", name);
+        return cryptoCurrService.getCrypto(name, pageable);
     }
 }
