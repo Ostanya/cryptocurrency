f5c600a3570695fed828047663c42e91d6ad0731
Osttanya <ostania.tania@gmail.com>
2021-11-22 19:39:51 +0200

    created cryptocurrency service with 3 get-methods

--- src/main/java/com/ostapenko/cryptocurrency/service/CryptoCurrService.java
+++ src/main/java/com/ostapenko/cryptocurrency/service/CryptoCurrService.java
@@ -3,6 +3,8 @@ package com.ostapenko.cryptocurrency.service;
 import com.ostapenko.cryptocurrency.entity.Crypto;
 import com.ostapenko.cryptocurrency.repository.TimerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,5 +15,14 @@ public class CryptoCurrService {
     public double getMinPrice(String name) {
         Crypto crypto = timerRepository.sortCurrencyByLprice(name);
         return crypto.getLprice();
+    }
+
+    public double getMaxPrice(String name) {
+        Crypto crypto = timerRepository.sortByDecsCurrencyByLprice(name);
+        return crypto.getLprice();
+    }
+
+    public Page<Crypto> getCrypto(String name, Pageable pageable) {
+        return timerRepository.findByCurr1(name, pageable);
     }
 }
